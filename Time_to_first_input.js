441f38fac8b375f04737c9ac8a0cb88dfdd14fd9
Cheng Chi (@joychester) <joychester@gmail.com>
2020-01-03 11:24:10 -0800

    Update Time_to_first_input.js

--- Time_to_first_input.js
+++ Time_to_first_input.js
@@ -1,23 +1,24 @@
-use 'strict';
+'use strict';
 
 let time_to_first_input = function() {
-  let perf = window.performance;
+  const perf = window.performance;
+  const start_time = perf.timing.navigationStart;
+  const current_time = perf.now();
+
   if(perf && perf.timing && perf.getEntriesByType) {
     let user_marks = perf.getEntriesByType('mark');
-    let last_user_mark = perf.getEntriesByType('mark')[user_marks.length - 1].startTime;
-    let user_start_interactive = perf.now();
+    let last_user_mark = user_marks[user_marks.length - 1].startTime;
 
     console.log(`user start to see our page at: ${last_user_mark}`);
-    console.log(`user start to interactive our page at: ${user_start_interactive}`);
+    console.log(`user start to interactive our page at: ${current_time}`);
 
-    //remove the eventlistener once user either conduct 'click' or 'scroll' on the page
-    window.removeEventListener('scroll', user_monitor_func);
-    window.removeEventListener('click', user_monitor_func);
   } else {
     console.log("performance object is not supported by current browser, remove the event listener anyway...");
-    window.removeEventListener('scroll', user_monitor_func);
-    window.removeEventListener('click', user_monitor_func);
   }
+  //remove the eventlistener once user either conduct 'click' or 'scroll' on the page
+  window.removeEventListener('scroll', time_to_first_input);
+  window.removeEventListener('click', time_to_first_input);
+
 };
 
 window.addEventListener('scroll', time_to_first_input);
