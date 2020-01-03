261d934873759bc0c025a05421469871c502ad0f
Cheng Chi (@joychester) <joychester@gmail.com>
2020-01-03 12:10:53 -0800

    Update Time_to_first_input.js

--- Time_to_first_input.js
+++ Time_to_first_input.js
@@ -1,16 +1,25 @@
 'use strict';
 
 let time_to_first_input = function() {
+
   const perf = window.performance;
-  const start_time = perf.timing.navigationStart;
-  const current_time = perf.now();
+  const current_time = Math.floor(perf.now());
+
+  console.log(`user first input since navigating to the page: ${current_time}`);
 
   if(perf && perf.timing && perf.getEntriesByType) {
-    let user_marks = perf.getEntriesByType('mark');
-    let last_user_mark = user_marks[user_marks.length - 1].startTime;
+    if (perf.getEntriesByType('mark')) {
+      const user_marks = perf.getEntriesByType('mark');
+      const last_user_mark_time = Math.floor(user_marks[user_marks.length - 1].startTime);
 
-    console.log(`user start to see our page at: ${last_user_mark}`);
-    console.log(`user start to interactive our page at: ${current_time}`);
+      console.log(`Performance mark duration since page navigation start: ${last_user_mark_time}`);
+      console.log(`User first input since lastest performance mark: ${current_time - last_user_mark_time}`)
+    }
+
+
+    const fcp_time = Math.floor(perf.getEntriesByName('first-contentful-paint')[0].startTime);
+    console.log(`First contentful paint duration since page navigation start: ${fcp_time}`);
+    console.log(`User first input since first contentful piant: ${current_time - fcp_time}`);
 
   } else {
     console.log("performance object is not supported by current browser, remove the event listener anyway...");
@@ -18,6 +27,8 @@ let time_to_first_input = function() {
   //remove the eventlistener once user either conduct 'click' or 'scroll' on the page
   window.removeEventListener('scroll', time_to_first_input);
   window.removeEventListener('click', time_to_first_input);
+
+  // will use the fetch API to bring those data back to logging server
 
 };
 
