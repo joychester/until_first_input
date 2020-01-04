3ec362597e4ec362e189f6ea73384f215b1fd8d4
Cheng Chi (@joychester) <joychester@gmail.com>
2020-01-03 16:18:23 -0800

    Update Time_to_first_input.js

--- Time_to_first_input.js
+++ Time_to_first_input.js
@@ -18,19 +18,20 @@ let time_to_first_input = function() {
       let last_user_mark_time = Math.floor(user_marks[user_marks.length - 1].startTime);
       console.log(`Performance mark duration since page navigation start: ${last_user_mark_time}`);
       console.log(`User first input since lastest performance mark: ${current_time - last_user_mark_time}`);
-
-      setTimeout(() => {
-        let fid_time = perf.getEntriesByType('first-input');
-        if(fid_time.length > 0) { // scoll is not included in 'first-input' event
-          let fid_start_time = Math.floor(fid_time[0].startTime);
-          let fid_process_start = Math.floor(fid_time[0].processingStart);
-          let fid_process_end = Math.floor(fid_time[0].processingEnd);
-          console.log(`First input event since page navigation start: ${fid_start_time}`);
-          console.log(`First input delay: ${fid_process_start-fid_start_time}`);
-          console.log(`First input processing time: ${fid_process_end-fid_process_start}`)
-        }
-      }, 2000);
     }
+    
+    setTimeout(() => {
+      let fid_time = perf.getEntriesByType('first-input');
+      if(fid_time.length > 0) { // scoll is not included in 'first-input' event
+        let fid_start_time = Math.floor(fid_time[0].startTime);
+        let fid_process_start = Math.floor(fid_time[0].processingStart);
+        let fid_process_end = Math.floor(fid_time[0].processingEnd);
+        console.log(`First input event since page navigation start: ${fid_start_time}`);
+        console.log(`First input delay: ${fid_process_start-fid_start_time}`);
+        console.log(`First input processing time: ${fid_process_end-fid_process_start}`)
+      }
+    }, 2000);
+    
   } else {
     console.log("performance object is not supported by current browser, remove the event listener anyway...");
   }
