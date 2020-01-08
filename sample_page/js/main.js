90f664e8bdad28a7e9716488d727dbeef36e69f8
Cheng Chi (@joychester) <joychester@gmail.com>
2020-01-07 21:52:02 -0800

    Update main.js

--- sample_page/js/main.js
+++ sample_page/js/main.js
@@ -1,31 +1,15 @@
 const iterations = 1000;
 const multiplier = 1000000000;
 
-//var worker = new Worker("js/calculate.js");
-
 /**
  * Doing the pointless computations.
  */
 var pointlessComputationsButton = document.getElementById("pointless-computations");
-pointlessComputationsButton.disabled = false;
 pointlessComputationsButton.addEventListener("click", doPointlessComputations, false);
 
 function doPointlessComputations() {
-  pointlessComputationsButton.disabled = true;
 
-  var useWorkerButton = document.getElementById("use-worker");
-  var useBlockingJsButton = document.getElementById("use-blocking-js");
-  var useRequestAnimationFrame = document.getElementById("use-request-animation-frame");
-
-  if (useBlockingJsButton.checked) {
-    doPointlessComputationsWithBlocking();
-  }
-  else if (useRequestAnimationFrame.checked) {
-    doPointlessComputationsWithRequestAnimationFrame();
-  }
-  else if (useWorkerButton.checked) {
-    doPointlessComputationsInWorker();
-  }
+  doPointlessComputationsWithBlocking();
 }
 
 /**
@@ -40,10 +24,8 @@ function startStop() {
   started = !started;
   if (started) {
     container.classList.add("started");
-    startStopButton.value = "Stop animations";
   }
   else {
    container.classList.remove("started");
-   startStopButton.value = "Start animations";
   }
 }
